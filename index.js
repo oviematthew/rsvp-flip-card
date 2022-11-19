0c304e5f21f57cc3c2e872f78b2f02ab94a4b036
oviematthew <90224141+oviematthew@users.noreply.github.com>
2022-11-19 16:31:57 +0100

    Add files via upload
    Added form fields
    Added transition animation on click of button to hide and reveal both cards respectively
    Added form validations

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 var myButton = document.getElementById("switch-btn");
-var myButtontwo = document.getElementById("back-btn");
 var topCont = document.getElementById("card-front");
 var bottomCont = document.getElementById("card-back");
 
 
 
+
+// All functions below occur on click of myButton variable called earlier
 myButton.addEventListener("click", function () {
-    topCont.classList.add("hidden") ;   
-    bottomCont.classList.remove("hidden");
+
+    // Add opacity and rotation to the front card
+    topCont.classList.add("opClass"); 
+    topCont.classList.add("rotate-card"); 
+    
+    setTimeout(function(){
+    
+    // Add hidden class to front card to hide it
+    topCont.classList.add("hidden");  
+    
+    // Remove hidden class to front card to display it
+    bottomCont.classList.remove("hidden"); 
+
+    // Add opacity class to reduce the opacity of the back card
+    bottomCont.classList.add("opClass"); 
+    },1000); 
+
+    setTimeout(function(){   
+    //Remove Card Opacity 
+    bottomCont.classList.add("re-opClass");
+    },1200); 
+
   });
 
   
 
-  myButtontwo.addEventListener("click", function () {
-    topCont.classList.remove("hidden");
-    bottomCont.classList.add("hidden")
-  });  
+
+  
--- mail.php
+++ mail.php
@@ -1,25 +1,30 @@
 <?php
-$subject = 'You Got Message'; // Subject of your email
+$subject = 'RSVP FORM'; // Subject of your email
 $to = 'ovieenams@gmail.com';  //Recipient's E-mail
 
 
 
 
 $fullname = $_REQUEST['fullname'];
+$country = $_REQUEST['country'];
 $guests = $_REQUEST['guests'];
+$nameofguest = $_REQUEST['nameofguest'];
 
 
-$email_from = $name.'<'.$email.'>';
+$email_from = $name.'<'.$subject.'>';
 
 
-$message .= 'Name : ' . $fullname . "\n";
-$message .= 'Number Of Guests : ' . $guests . "\n";
+$message .= 'Full Name : ' . $fullname . "\n";
+$message .= 'Country Of Residence : ' . $country . "\n";
+$message .= 'Number Of Guest(s) : ' . $guests . "\n";
+$message .= 'Name Of Guest(s) : ' . $nameofguest . "\n";
+
 
 
 if (@mail($to, $subject, $message, $email_from))
 {
 	// Transfer the value 'sent' to ajax function for showing success message.
-	header("Location: index.html");
+	header("Location: success.html");
     
     echo("Filled Successfully");
 }
